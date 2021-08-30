fabbdfc3c0509fc8ed32a0488d72c51051935c5c
Jakub Skoneczny <jakub.skoneczny@netguru.com>
2021-08-30 20:25:43 +0200

    fix(backend): remove unnecessary route from credit module

--- apps/backend/src/credit/credit.controller.ts
+++ apps/backend/src/credit/credit.controller.ts
@@ -1,9 +1,7 @@
 import {
   Controller,
-  Post,
   Get,
   HttpStatus,
-  Body,
   Request,
   HttpException,
 } from '@nestjs/common';
@@ -12,10 +10,8 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { RequestWithUser } from '../common/interface/request-with-user.interface';
 import { Authorised } from '../auth/auth.decorator';
 import { CreditService } from './credit.service';
-import { CreateCreditDto } from './dto/create-credit.dto';
 
 import { ReadCreditError } from './error/read-credit.error';
-import { CreateCreditError } from './error/create-credit.error';
 import { CreditDto } from './dto/credit.dto';
 
 @Controller('credits')
@@ -47,48 +43,6 @@ export class CreditController {
       return credits.map((credit) => CreditDto.createDtoFromEntity(credit));
     } catch (error) {
       if (error instanceof ReadCreditError) {
-        throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
-      }
-
-      throw new HttpException(
-        'Internal server error',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
-
-  @Post('/')
-  @Authorised()
-  @ApiTags('credits')
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'Credit has been created',
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'Bad request',
-  })
-  @ApiResponse({
-    status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: 'Internal server error',
-  })
-  public async createCredit(
-    @Body() createCreditDto: CreateCreditDto,
-    @Request() request: RequestWithUser,
-  ): Promise<CreditDto> {
-    try {
-      const user = request.user;
-      const { limit, duration } = createCreditDto;
-
-      const credit = await this.creditService.createCredit(
-        limit,
-        duration,
-        user,
-      );
-
-      return CreditDto.createDtoFromEntity(credit);
-    } catch (error) {
-      if (error instanceof CreateCreditError) {
         throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
       }
 
